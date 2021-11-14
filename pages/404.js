89b2cb33dbafc16c05da760fc00a6c21f77f0137
Martin Rocek <martin@rocek.dev>
2021-11-14 13:05:59 +0100

    ⚗ (404): return 404

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import Head from '@components/Head'
+
+const Error404 = () => {
+  return (
+    <main className='mainContainer'>
+      <Head title='Not Found | humanities.tools' />
+      <div className='content__error'>
+        <h1>404</h1>
+        <p>Page not found</p>
+      </div>
+    </main>
+  )
+}
+
+export default Error404
