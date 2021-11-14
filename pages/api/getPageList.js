24967ab4ee3a62f5f28986f0b8713e2a683d4a38
Martin Rocek <martin@rocek.dev>
2021-11-14 13:30:12 +0100

    ⚗ remove content

--- pages/api/getPageList.js
+++ pages/api/getPageList.js
@@ -3,27 +3,27 @@ import path from 'path'
 import matter from 'gray-matter'
 
 export default function handler (req, res) {
-  const dirRelativeToPublicFolder = 'pages'
-  const locale = req.query.locale || 'any'
+  // const dirRelativeToPublicFolder = 'pages'
+  // const locale = req.query.locale || 'any'
 
-  const dir = path.resolve('./content', dirRelativeToPublicFolder)
+  // const dir = path.resolve('./content', dirRelativeToPublicFolder)
 
-  let filenames = fs.readdirSync(dir);
+  // let filenames = fs.readdirSync(dir);
 
-  if (locale !== 'any') {
-    filenames = filenames.filter(file => file.includes(`.${locale}.`))
-  }
+  // if (locale !== 'any') {
+  //   filenames = filenames.filter(file => file.includes(`.${locale}.`))
+  // }
 
-  const pages = filenames.map(filename => {
-    const filePath = path.join(dir, filename)
-    const fileContents = fs.readFileSync(filePath, 'utf8')
-    const { data } = matter(fileContents)
-    return {
-      slug: filename.replace(/^.*[\\\/]/, '').slice(0, -6),
-      ...data
-    }
-  })
+  // const pages = filenames.map(filename => {
+  //   const filePath = path.join(dir, filename)
+  //   const fileContents = fs.readFileSync(filePath, 'utf8')
+  //   const { data } = matter(fileContents)
+  //   return {
+  //     slug: filename.replace(/^.*[\\\/]/, '').slice(0, -6),
+  //     ...data
+  //   }
+  // })
 
-  res.statusCode = 200
-  res.json(pages);
+  // res.statusCode = 200
+  res.json('Hi');
 }
