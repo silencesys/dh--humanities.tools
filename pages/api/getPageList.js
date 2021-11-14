95e073fe4629305a56aa2891d2a7edfa343cc123
Martin Rocek <martin@rocek.dev>
2021-11-14 13:04:32 +0100

    ⚗ (404): remove custom 404 and return api

--- pages/api/getPageList.js
+++ pages/api/getPageList.js
@@ -1,31 +1,31 @@
-// import fs from 'fs'
-// import path from 'path'
-// import matter from 'gray-matter'
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
 
-// const getPageList = (req, res) => {
-//   const dirRelativeToPublicFolder = 'pages'
-//   const locale = req.query.locale || 'any'
+const getPageList = (req, res) => {
+  const dirRelativeToPublicFolder = 'pages'
+  const locale = req.query.locale || 'any'
 
-//   const dir = path.resolve('./content', dirRelativeToPublicFolder)
+  const dir = path.resolve('./content', dirRelativeToPublicFolder)
 
-//   let filenames = fs.readdirSync(dir);
+  let filenames = fs.readdirSync(dir);
 
-//   if (locale !== 'any') {
-//     filenames = filenames.filter(file => file.includes(`.${locale}.`))
-//   }
+  if (locale !== 'any') {
+    filenames = filenames.filter(file => file.includes(`.${locale}.`))
+  }
 
-//   const pages = filenames.map(filename => {
-//     const filePath = path.join(dir, filename)
-//     const fileContents = fs.readFileSync(filePath, 'utf8')
-//     const { data } = matter(fileContents)
-//     return {
-//       slug: filename.replace(/^.*[\\\/]/, '').slice(0, -6),
-//       ...data
-//     }
-//   })
+  const pages = filenames.map(filename => {
+    const filePath = path.join(dir, filename)
+    const fileContents = fs.readFileSync(filePath, 'utf8')
+    const { data } = matter(fileContents)
+    return {
+      slug: filename.replace(/^.*[\\\/]/, '').slice(0, -6),
+      ...data
+    }
+  })
 
-//   res.statusCode = 200
-//   res.json(pages);
-// }
+  res.statusCode = 200
+  res.json(pages);
+}
 
-// export default getPageList
+export default getPageList
