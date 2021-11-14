13c762c4692e5174939b39256fef97c9028a05c3
Martin Rocek <martin@rocek.dev>
2021-11-14 12:53:27 +0100

    🐛 (error): fix error page header/footer

--- pages/404.js
+++ pages/404.js
@@ -1,20 +1,14 @@
 import Head from '@components/Head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
 
 const Error404 = () => {
   return (
-    <div className='websiteWrapper'>
+    <main className='mainContainer'>
       <Head title='Not Found | humanities.tools' />
-      <main className='mainContainer'>
-        <Header />
-        <div className='content__error'>
-          <h1>404</h1>
-          <p>Page not found</p>
-        </div>
-      </main>
-      <Footer />
-    </div>
+      <div className='content__error'>
+        <h1>404</h1>
+        <p>Page not found</p>
+      </div>
+    </main>
   )
 }
 
