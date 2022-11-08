0d5e2d3847c7fb99912b0e213e5a527690bcdef4
Martin Rocek <martin@rocek.dev>
2022-11-08 23:27:08 +0100

    📈 (umami): add analytics

--- components/Head.js
+++ components/Head.js
@@ -6,24 +6,24 @@ const HeadElement = ({
 }) => {
   return (
     <Head>
-    <title>{title}</title>
-    <meta name='description' content='Catalogue of tools and resources for academics in the field of humanities.' />
-    <meta name='keywords' content='humanities, tools, tools for humanities, digital humanities, history, philosphy, arts, liberal arts, linguistic, languages, education, academia, academic tools' />
-    <link rel='icon' href='/favicon.ico' />
-    <link rel="stylesheet" href="https://use.typekit.net/wbs7hla.css" />
-    <meta property='og:title' content={title} />
-    <meta property='og:description' content={description} />
-    <meta property='og:type' content='website' />
-    <meta property='og:site_name' content='Humanities.tools' />
-    <meta property='og:url' content='https://www.humanities.tools' />
-    <meta property='og:image' content='https://www.humanities.tools/og-image.jpg' />
-    <meta property='og:image:width' content='1200' />
-    <meta property='og:image:height' content='630' />
-    <meta name='twitter:image' content='https://www.humanities.tools/og-image.jpg' />
-    <meta name='twitter:card' content='summary_large_image' />
-    <meta name='twitter:image:alt' content='Catalogue of tools and resources for academics in the field of humanities' />
-    <meta name="twitter:creator" content="@silencesys" />
-  </Head>
+      <title>{title}</title>
+      <meta name='description' content='Catalogue of tools and resources for academics in the field of humanities.' />
+      <meta name='keywords' content='humanities, tools, tools for humanities, digital humanities, history, philosphy, arts, liberal arts, linguistic, languages, education, academia, academic tools' />
+      <link rel='icon' href='/favicon.ico' />
+      <link rel="stylesheet" href="https://use.typekit.net/wbs7hla.css" />
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={description} />
+      <meta property='og:type' content='website' />
+      <meta property='og:site_name' content='Humanities.tools' />
+      <meta property='og:url' content='https://www.humanities.tools' />
+      <meta property='og:image' content='https://www.humanities.tools/og-image.jpg' />
+      <meta property='og:image:width' content='1200' />
+      <meta property='og:image:height' content='630' />
+      <meta name='twitter:image' content='https://www.humanities.tools/og-image.jpg' />
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:image:alt' content='Catalogue of tools and resources for academics in the field of humanities' />
+      <meta name="twitter:creator" content="@silencesys" />
+    </Head>
   )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import Layout from '@components/Layout'
+import Script from 'next/script'
 // Add custom styles
 import '@style/variables.css'
 import '@style/globals.css'
@@ -12,6 +13,12 @@ config.autoAddCss = false
 function HumanitiesTools({ Component, pageProps }) {
   return (
     <Layout>
+      <Script
+        async
+        defer
+        data-website-id="a90b4937-dd40-4246-87fa-2cd2e5ba01b4"
+        src="https://umami.rocek.dev/umami.js"
+      />
       <Component {...pageProps} />
     </Layout>
   )
